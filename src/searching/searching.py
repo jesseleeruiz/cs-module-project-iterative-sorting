3dca104ae27b0528b689cc55c3f1411f07b2ccfd
jesseleeruiz <jesseleeruiz@gmail.com>
2020-07-20 12:19:29 -1000

    Completed Binary Search. Test passes.

--- src/searching/searching.py
+++ src/searching/searching.py
@@ -2,14 +2,15 @@ def linear_search(arr, target):
     for number in arr:
         if target == number:
             return arr.index(number)
-
     return -1   # not found
 
 
 # Write an iterative implementation of Binary Search
 def binary_search(arr, target):
-
-    # Your code here
-
-
+    arr_length = len(arr)
+    while arr_length > 0:
+        for number in arr:
+            if target == number:
+                return arr.index(number)
+        arr_length /= 2
     return -1  # not found
